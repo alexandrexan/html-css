dc2825d4155b04a7856295111f102a593c77020d
alexandrexan <xampsbeatz@gmail.com>
2025-12-01 21:23:55 -0300

    ex026 mq005
    Site responsivo com menu hamburguer para dispositivos móveis

---
+++ exercicios/ex026/mq005/menu-burguer.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+    const menu = document.querySelector('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
